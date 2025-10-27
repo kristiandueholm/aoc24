b4ccfb7b0c8fb12994df8c6ac543c7e86472f50c
Kristian Dueholm Hill <kristian618@gmail.com>
2025-10-27 11:21:08 +0100

    Finish day 11.

--- 11/11.go
+++ 11/11.go
@@ -8,6 +8,14 @@ import (
 	"strconv"
 )
 
+func getCount(counts map[int]int) int {
+	count := 0
+	for _, v := range counts {
+		count += v
+	}
+	return count
+}
+
 func main() {
 	input, err := os.Open("input.txt")
 	if err != nil {
@@ -16,31 +24,46 @@ func main() {
 	scanner := bufio.NewScanner(input)
 	scanner.Split(bufio.ScanWords)
 
-	stones := make([]int, 0)
+	stones := make(map[int]int, 0)
 	for scanner.Scan() {
 		stone, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			log.Fatal(err)
 		}
-		stones = append(stones, stone)
+		stones[stone]++
 	}
 
-	for i, stone := range stones {
-		stoneString := strconv.Itoa(stone)
-		nDigits := len(stoneString)
-		if stone == 0 {
-			stones[i] = 1
-		} else if nDigits%2 == 0 && nDigits > 0 {
-			mid := nDigits / 2
-			left, errL := strconv.Atoi(stoneString[:mid])
-			if errL != nil {
-				log.Fatal(errL)
-			}
-			right, errR := strconv.Atoi(stoneString[mid:])
-			if errR != nil {
-				log.Fatal(errR)
-			}
-			// mutating length of the array I am iterating over??
+	iterations := 75
+	var part1 int
+
+	for i := range iterations {
+		if i == 25 {
+			part1 = getCount(stones)
 		}
+		temp := make(map[int]int, 0)
+		for stone, count := range stones {
+			stoneString := strconv.Itoa(stone)
+			nDigits := len(stoneString)
+			if stone == 0 {
+				temp[1] += count
+			} else if nDigits%2 == 0 && nDigits > 0 {
+				mid := nDigits / 2
+				left, errL := strconv.Atoi(stoneString[:mid])
+				if errL != nil {
+					log.Fatal(errL)
+				}
+				right, errR := strconv.Atoi(stoneString[mid:])
+				if errR != nil {
+					log.Fatal(errR)
+				}
+				temp[left] += count
+				temp[right] += count
+			} else {
+				temp[stone*2024] += count
+			}
+		}
+		stones = temp
 	}
+	fmt.Printf("Part 1: %d\n", part1)
+	fmt.Printf("Part 2: %d\n", getCount(stones))
 }
